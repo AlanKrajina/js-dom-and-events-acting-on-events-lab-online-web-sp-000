41c94eed6249b4b552ec4aa316878b51b0913bfa
Alan Krajina <alankrajina88@gmail.com>
2019-09-22 11:04:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function addNewElementAsLi(){
 function addNewLiOnClick(){
   let x =  addNewElementAsLi();
 
-  addEventListener('keydown', function(e) {
+  x.addEventListener('keydown', function(e) {
     console.log(e.which);
   });
 }
