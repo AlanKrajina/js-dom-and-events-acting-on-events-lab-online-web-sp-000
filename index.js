31e8c533f87ce48e87b13c6a2689144552ae10f9
Alan Krajina <alankrajina88@gmail.com>
2019-09-22 11:36:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ function addNewElementAsLi(){
 function addNewLiOnClick(){
 //  document.body.addEventListener('click', addNewElementAsLi)
 
-  let x = document.getElementById("myForm").elements[0].value
+   document.getElementById("myForm").elements[0].value="";
 
-  document.body.addEventListener('click', function() {
-  return x = "";
-  });
+//  document.body.addEventListener('click', function() {
+//  return x = "";
+//  });
 
 }
 
